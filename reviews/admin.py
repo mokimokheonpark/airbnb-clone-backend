7714e9c7b2559605eee6948e8e90aaa7f6e0b0e9
Mokheon Park <mokimokheonpark@gmail.com>
2023-07-05 15:57:49 -0400

    added a customized filter

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,5 +1,24 @@
+from typing import Any, Optional
 from django.contrib import admin
+from django.db.models.query import QuerySet
 from .models import Review
+
+
+class WordFilter(admin.SimpleListFilter):
+    title = "word in review"
+    parameter_name = "word"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("good", "Good"),
+            ("great", "Great"),
+            ("awesome", "Awesome"),
+        ]
+
+    def queryset(self, request, reviews):
+        word = self.value()
+        if word:
+            return reviews.filter(review__contains=word)
 
 
 @admin.register(Review)
@@ -10,4 +29,10 @@ class ReviewAdmin(admin.ModelAdmin):
         "review",
     )
 
-    list_filter = ("rating",)
+    list_filter = (
+        WordFilter,
+        "rating",
+        "user__is_host",
+        "room__category",
+        "room__pet_friendly",
+    )

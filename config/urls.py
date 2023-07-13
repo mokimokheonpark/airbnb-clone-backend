90e2f7cb92413192180707aabf0b8e68c0bb26c4
Mokheon Park <mokimokheonpark@gmail.com>
2023-07-13 16:10:40 -0400

    experiences app REST API

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/categories/", include("categories.urls")),
+    path("api/v1/experiences/", include("experiences.urls")),
     path("api/v1/rooms/", include("rooms.urls")),
 ]

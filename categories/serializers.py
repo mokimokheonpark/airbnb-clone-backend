dc68843b4a383c19e398fcd940c21191b68a57ea
Mokheon Park <mokimokheonpark@gmail.com>
2023-07-13 17:46:32 -0400

    rooms app Rooms, RoomDetail GET API

--- categories/serializers.py
+++ categories/serializers.py
@@ -5,4 +5,7 @@ from .models import Category
 class CategorySerializer(ModelSerializer):
     class Meta:
         model = Category
-        fields = "__all__"
+        fields = (
+            "name",
+            "category_kind",
+        )
--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,8 +1,35 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Amenity
+from .models import Amenity, Room
+from categories.serializers import CategorySerializer
+from users.serializers import TinyUserSerializer
+
+
+class RoomListSerializer(ModelSerializer):
+    class Meta:
+        model = Room
+        fields = (
+            "pk",
+            "name",
+            "country",
+            "city",
+            "price",
+        )
 
 
 class AmenitySerializer(ModelSerializer):
     class Meta:
         model = Amenity
+        fields = (
+            "name",
+            "description",
+        )
+
+
+class RoomDetailSerializer(ModelSerializer):
+    class Meta:
+        model = Room
         fields = "__all__"
+
+    amenities = AmenitySerializer(many=True)
+    owner = TinyUserSerializer()
+    category = CategorySerializer()

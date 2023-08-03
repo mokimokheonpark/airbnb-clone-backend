b3b4cc8a403a126094e4afa42f2facb0123122a0
Mokheon Park <mokimokheonpark@gmail.com>
2023-08-03 13:01:21 -0400

    wishlists app WishlistDetail GET, PUT, DELETE API

--- wishlists/views.py
+++ wishlists/views.py
@@ -1,5 +1,7 @@
+from rest_framework.exceptions import NotFound
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
 from rest_framework.views import APIView
 from .models import Wishlist
 from .serializers import WishlistSerializer
@@ -24,3 +26,42 @@ class Wishlists(APIView):
         new_wishlist = serializer.save(user=request.user)
         serializer = WishlistSerializer(new_wishlist)
         return Response(serializer.data)
+
+
+class WishlistDetail(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self, pk, user):
+        try:
+            return Wishlist.objects.get(pk=pk, user=user)
+        except Wishlist.DoesNotExist:
+            raise NotFound
+
+    def get(self, request, pk):
+        wishlist = self.get_object(pk, request.user)
+        serializer = WishlistSerializer(
+            wishlist,
+            context={"request": request},
+        )
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        wishlist = self.get_object(pk, request.user)
+        serializer = WishlistSerializer(
+            wishlist,
+            data=request.data,
+            partial=True,
+        )
+        if not serializer.is_valid():
+            return Response(serializer.erros)
+        updated_wishlist = serializer.save()
+        serializer = WishlistSerializer(
+            updated_wishlist,
+            context={"request": request},
+        )
+        return Response(serializer.data)
+
+    def delete(self, request, pk):
+        wishlist = self.get_object(pk, request.user)
+        wishlist.delete()
+        return Response(status=HTTP_200_OK)

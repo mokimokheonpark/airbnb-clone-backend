73e1e643b9a0017f3b71fecc080d99d9c4ce2d58
Mokheon Park <mokimokheonpark@gmail.com>
2023-08-07 14:50:53 -0400

    users app Users POST API

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserProfile
+from .views import Users, UserProfile
 
 urlpatterns = [
+    path("", Users.as_view()),
     path("profile", UserProfile.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,25 @@
+from rest_framework.exceptions import ParseError
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import PrivateUserSerializer
+
+
+class Users(APIView):
+    def post(self, request):
+        raw_password = request.data.get("password")
+        if not raw_password:
+            raise ParseError
+
+        serializer = PrivateUserSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(serializer.errors)
+
+        new_user = serializer.save()
+        new_user.set_password(raw_password)
+        new_user.save()
+        serializer = PrivateUserSerializer(new_user)
+        return Response(serializer.data)
 
 
 class UserProfile(APIView):

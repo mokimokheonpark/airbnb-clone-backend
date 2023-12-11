665c273a6c7e3aaf6f30e77831f3ab67c556c7d8
Mokheon Park <mokimokheonpark@gmail.com>
2023-12-11 17:14:17 -0500

    image upload test from local

--- medias/views.py
+++ medias/views.py
@@ -1,3 +1,5 @@
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
 from rest_framework.exceptions import NotFound, PermissionDenied
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -24,3 +26,20 @@ class PhotoDetail(APIView):
 
         photo.delete()
         return Response(status=HTTP_200_OK)
+
+
+class GetUploadURL(APIView):
+    def post(self, request):
+        return Response(
+            {
+                "id": "00000",
+                "uploadURL": "http://127.0.0.1:8000/api/v1/medias/photos/upload-photo",
+            }
+        )
+
+
+class UploadPhoto(APIView):
+    def post(self, request):
+        image_data = request.FILES["file"]
+        path = default_storage.save(image_data, ContentFile(image_data.read()))
+        return Response({"result": {"id": path}})

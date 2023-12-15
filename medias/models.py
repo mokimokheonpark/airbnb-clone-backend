32b22dbac5e87ade709e250d763b57b4ed6902c9
Mokheon Park <mokimokheonpark@gmail.com>
2023-12-15 17:46:25 -0500

    connected to AWS S3

--- medias/models.py
+++ medias/models.py
@@ -1,12 +1,18 @@
 from django.db import models
+from storages.backends.s3boto3 import S3Boto3Storage
 from common.models import CommonModel
+
+
+class S3MediaStorage(S3Boto3Storage):
+    location = "photos"
+    file_overwrite = False
 
 
 class Photo(CommonModel):
     def __str__(self) -> str:
         return f"{self.room}"
 
-    file = models.ImageField()
+    file = models.ImageField(storage=S3MediaStorage())
 
     room = models.ForeignKey(
         "rooms.Room",

99a42b569793767a4d7026e9eb192b40265551bf
Mokheon Park <mokimokheonpark@gmail.com>
2024-02-05 18:32:39 -0500

    debugging for photo-upload

--- medias/views.py
+++ medias/views.py
@@ -1,7 +1,8 @@
 from rest_framework.exceptions import NotFound, PermissionDenied
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.parsers import FileUploadParser
 from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 from rest_framework.views import APIView
 from .models import Photo
 from .serializers import PhotoSerializer, PhotoDetailSerializer
@@ -44,3 +45,16 @@ class PhotoDetail(APIView):
 
         photo.delete()
         return Response(status=HTTP_200_OK)
+
+
+class PhotoUpload(APIView):
+    parser_classes = (FileUploadParser,)
+
+    def post(self, request, *args, **kwargs):
+        file_serializer = PhotoSerializer(data=request.data)
+
+        if file_serializer.is_valid():
+            file_serializer.save()
+            return Response(file_serializer.data, status=HTTP_201_CREATED)
+        else:
+            return Response(file_serializer.errors, status=HTTP_400_BAD_REQUEST)

d6e9b818402d84f46ea87e4b9befeed56a2994c0
Mokheon Park <mokimokheonpark@gmail.com>
2024-02-09 16:40:51 -0500

    implemented Photos POST API

--- medias/views.py
+++ medias/views.py
@@ -1,8 +1,7 @@
 from rest_framework.exceptions import NotFound, PermissionDenied
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework.parsers import FileUploadParser
+from rest_framework.permissions import IsAuthenticatedOrReadOnly    
 from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST
+from rest_framework.status import HTTP_200_OK
 from rest_framework.views import APIView
 from .models import Photo
 from .serializers import PhotoSerializer, PhotoDetailSerializer
@@ -18,6 +17,14 @@ class Photos(APIView):
             many=True,
             context={"request": request},
         )
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = PhotoSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(serializer.errors)
+        new_photo = serializer.save()
+        serializer = PhotoSerializer(new_photo)
         return Response(serializer.data)
 
 
@@ -45,16 +52,4 @@ class PhotoDetail(APIView):
 
         photo.delete()
         return Response(status=HTTP_200_OK)
-
-
-class PhotoUpload(APIView):
-    parser_classes = (FileUploadParser,)
-
-    def post(self, request, *args, **kwargs):
-        file_serializer = PhotoSerializer(data=request.data)
-
-        if file_serializer.is_valid():
-            file_serializer.save()
-            return Response(file_serializer.data, status=HTTP_201_CREATED)
-        else:
-            return Response(file_serializer.errors, status=HTTP_400_BAD_REQUEST)
+    

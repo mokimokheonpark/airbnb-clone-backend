04eb1713ba26886fa6d7a8070b7e1b8aae00ce85
Mokheon Park <mokimokheonpark@gmail.com>
2024-02-26 16:18:00 -0500

    did domain settings

--- config/settings.py
+++ config/settings.py
@@ -212,3 +212,10 @@ AWS_S3_REGION_NAME = "us-east-2"
 AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+
+# Domain
+
+if not DEBUG:
+    SESSION_COOKIE_DOMAIN = ".airbnbcloneproject.xyz"
+    CSRF_COOKIE_DOMAIN = ".airbnbcloneproject.xyz"
